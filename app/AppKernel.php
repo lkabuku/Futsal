a0fd4e1dda0c1504cfa422d2a38aefde844d6a90
Lucien Kabuku <lucien.kabuku@niji.fr>
2015-03-24 15:38:13 -0700

    Add bundle TournamentBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Futsal\UserBundle\FutsalUserBundle(),
+            new Futsal\TournamentBundle\FutsalTournamentBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
