c12bce94f41cb9874bdb12cf6a4d3e6955147392
lkabuku <lucien.kabuku@gmail.com>
2015-03-26 00:51:43 +0100

    Update SecurityBundle

--- src/Futsal/UserBundle/Command/UserAddCommand.php
+++ src/Futsal/UserBundle/Command/UserAddCommand.php
@@ -12,8 +12,24 @@ class UserAddCommand  extends Command
 {
     protected function configure()
     {
-        
+        $this
+            ->setName('user:add')
+            ->setDescription('Add user in the back office')
+            ->addArgument('user_values', InputArgument::IS_ARRAY, 'Tape "username", "password", "email" ')
+            ->addOption('errors', null, InputOption::VALUE_NONE, 'Si définie, la tâche affichera les erreurs')
+        ;
     }
-    
-    
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userValues = $input->getArgument('user_values');
+        
+        $text = implode($userValues, ",");
+
+        if ($input->getOption('errors')) {
+            $text = strtoupper($text);
+        }
+
+        $output->writeln($text);
+    }
 }
--- src/Futsal/UserBundle/Controller/SecurityController.php
+++ src/Futsal/UserBundle/Controller/SecurityController.php
@@ -3,46 +3,65 @@
 namespace Futsal\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+
+use Futsal\UserBundle\Entity\User;
 
 class SecurityController extends Controller
 {
     
-    protected function LoginAction(Request $request)
+    public function loginAction(Request $request)
     {
         // Si le visiteur est déjà identifié, on le redirige vers l'accueil
+        /*
         if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {            
             return $this->redirect($this->generateUrl('futsal_user_admin_homepage'));
         }
-
+        */
+        
         $session = $request->getSession();
         
         // On vérifie s'il y a des erreurs d'une précédente soumission du formulaire
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
         } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+            $error = '';
         }
+
+        $lastUsername = (null === $session) ? '' : $session->get(SecurityContextInterface::LAST_USERNAME);
         
         return $this->render('FutsalUserBundle:Security:login.html.twig', array(
             // Valeur du précédent nom d'utilisateur entré par l'internaute
-            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'last_username' => $lastUsername,
             'error'         => $error,
         ));
     }
     
-    protected function LoginCheckAction(Request $request)
+    public function loginCheckAction(Request $request)
+    {
+        $username = $request->request->get("username");
+        $plaintextPassword = $request->request->get("password");
+        
+        $user = new User();
+        $encoder = $this->container->get('security.password_encoder');
+        $password = $encoder->encodePassword($user, $plaintextPassword);
+        
+        $em = $this->getDoctrine()->getManager();
+        $userRepository = $em->getRepository('FutsalUserBundle:User');
+        $userRepository->checkUserByUsernameAndPassword($username, $password);
+    }
+    
+    public function logoutAction(Request $request)
     {
         
     }
     
-    protected function LogoutAction(Request $request)
-    {
-        
-    }
-    
-    protected function AdminAction(Request $request)
+    public function adminAction(Request $request)
     {
         return new Response("Admin Section !");
     }
