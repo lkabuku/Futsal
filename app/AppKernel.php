0f1cd6ec8d8dd2cf1a5ec3d25aebfbb1307c0736
Lucien Kabuku <lucien.kabuku@niji.fr>
2015-03-27 18:17:49 +0100

    Sonata Admin Class

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,8 +23,8 @@ class AppKernel extends Kernel
             new Sonata\CoreBundle\SonataCoreBundle(),
             new Sonata\BlockBundle\SonataBlockBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
-            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
             new Sonata\AdminBundle\SonataAdminBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
