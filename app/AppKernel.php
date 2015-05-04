68da451f809b6e118bb47472d0161321d0ba42f3
Lucien Kabuku <lucien.kabuku@niji.fr>
2015-05-04 15:21:02 +0200

    Ajout du bundle BraincraftedBootstrapBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,8 @@ class AppKernel extends Kernel
             
             new FOS\UserBundle\FOSUserBundle(),
             new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            
+            new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
